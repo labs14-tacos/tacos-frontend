48d4cdcb34d78d64291d24b66839b7b11ee02024
meginks <meganjones.dev@gmail.com>
2019-07-11 16:14:57 -0700

    created components, added env to gitignore

---
+++ client/components/UpdateUserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+// this can be used as the update area for the logged-in person's profile info
+class UpdateUserProfile extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default UpdateUserProfile;
