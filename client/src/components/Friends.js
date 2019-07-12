13db5ff3a79f1a1a223baf72926a970fa6ed8326
meginks <meganjones.dev@gmail.com>
2019-07-12 10:07:34 -0700

    added component to display users

---
+++ client/src/components/Friends.js
@@ -0,0 +1,32 @@
+import React from 'react'; 
+import axios from 'axios';
+
+// I am naming this component Friends so we can repurpose it to later be the display of each friend a user is friends with. For now (7-11) this is just to pull in our seed data to prove our FE and BE are communicating. -- MJ
+
+
+
+
+class Friends extends React.Component {
+state = {
+    friends: [
+       
+    ]
+}
+
+componentDidMount() {
+     axios.get(process.env.REACT_APP_BACKEND_URL_USERS).then(res => this.setState({friends: res.data})).catch(error => console.log(error));
+}
+
+render() {
+ return (
+     <div>
+         {
+             this.state.friends.map(friend => <p>{friend.firstName} {friend.lastName}</p>)
+         }
+     </div>
+ )
+
+};
+
+}
+export default Friends;
