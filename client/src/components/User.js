06333fb10c6c9d5842cafc0420e44831e59e9223
Frankzhac <Frank.hemle@gmail.com>
2019-07-25 13:55:48 -0400

    user component done

---
+++ client/src/components/User.js
@@ -0,0 +1,26 @@
+import React from 'react'; 
+// import UserAvatar from './UserAvatar';
+
+// This can be used to display the person who is logged in's own profile information
+const User = props => {
+
+    return (
+        <div>
+            <div className = "user-profile">
+                <h1>Profile</h1>
+                {/* <UserAvatar /> */}
+                <h4>{props.name}</h4>
+                <h4>{props.age} years old</h4>
+                <h4>{props.email}</h4>
+            </div>
+        </div>
+    )
+}
+
+User.defaultProps = {
+name: '',
+age: '',
+email: ''
+}
+
+export default User;
