b3a8726e0a6023ef70c8bebead391d61299fbdb1
anthony <anthony24.kwhomes@gmail.com>
2019-08-05 18:17:23 -0700

    Update forms for UserProfile and TacoLog

--- client/src/components/UpdateUserProfile.js
+++ client/src/components/UpdateUserProfile.js
@@ -2,13 +2,160 @@ import React from 'react';
 
 // this can be used as the update area for the logged-in person's profile info
 class UpdateUserProfile extends React.Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            userPhoto: "",
+            zipCode: "",
+            tacosPerMonth: "",
+            hardOrSoft: "",
+            cornOrFlour: "",
+            heatPreference: "",
+            streetOrGourmet: "",
+            favTaco: "",
+            favTacoLocation: "",
+            bestTacoMemory: "",
+            instaHandle: "",
+            twitterHandle: "",
+            facebookPage: "",
+            website: ""
+        };
     }
-}
+    
+    handleChange = event => {
+        this.ListeningStateChangedEvent({ [event.target.name]: event.target.value });
+    };
+    
+    updateProfile = event => {
+        event.preventDefault();
+        const id = this.props.match.params.id;
+        const userProfile = {
+            ...this.state
+        };
+        this.props.UpdateUserProfile(id, userProfile);
+        this.props.history.push('/');
+    };
+    
+    render() {
+        return(
+            <>
+                <div>
+                    <h2 className="form-heading">Update Taco Log</h2>
+                    <form className="edit-form">
+                        <input
+                            type="text"
+                            name="firstName"
+                            onChange={this.handleChange}
+                            placeholder="First Name"
+                        />
+                        <input
+                            type="text"
+                            name="lastName"
+                            onChange={this.handleChange}
+                            placeholder="Last Name"
+                        />
+                        <input
+                            type="text"
+                            name="email"
+                            onChange={this.handleChange}
+                            placeholder="Email"
+                        />
+                        <input
+                            type="text"
+                            name="userPhoto"
+                            onChange={this.handleChange}
+                            placeholder="Img url"
+                        />
+                        <input
+                            type="text"
+                            name="zipCode"
+                            onChange={this.handleChange}
+                            placeholder="Zip Code"
+                        />
+                        <input
+                            type="text"
+                            name="tacosPerMonth"
+                            onChange={this.handleChange}
+                            placeholder="Tacos Per Month"
+                        />
+                        <input
+                            type="text"
+                            name="hardOrSoft"
+                            onChange={this.handleChange}
+                            placeholder="Hard or Soft"
+                        />
+                        <input
+                            type="text"
+                            name="cornOrFlour"
+                            onChange={this.handleChange}
+                            placeholder="Corn or Flour"
+                        />
+                        <input
+                            type="text"
+                            name="heatPreference"
+                            onChange={this.handleChange}
+                            placeholder="Heat Preference"
+                        />
+                        <input
+                            type="text"
+                            name="streetOrGourmet"
+                            onChange={this.handleChange}
+                            placeholder="Street or Gourmet"
+                        />
+                        <input
+                            type="text"
+                            name="favTaco"
+                            onChange={this.handleChange}
+                            placeholder="Favorite Taco"
+                        />
+                        <input
+                            type="text"
+                            name="favTacoLocation"
+                            onChange={this.handleChange}
+                            placeholder="Favorite Taco Location"
+                        />
+                        <input
+                            type="text"
+                            name="bestTacoMemory"
+                            onChange={this.handleChange}
+                            placeholder="Best Taco Memory"
+                        />
+                        <input
+                            type="text"
+                            name="instaHandle"
+                            onChange={this.handleChange}
+                            placeholder="Instagram Account Here"
+                        />
+                        <input
+                            type="text"
+                            name="twitterHandle"
+                            onChange={this.handleChange}
+                            placeholder="Twitter Handle Here"
+                        />
+                        <input
+                            type="text"
+                            name="facebookPage"
+                            onChange={this.handleChange}
+                            placeholder="Facebook Page Here"
+                        />
+                        <input
+                            type="text"
+                            name="website"
+                            onChange={this.handleChange}
+                            placeholder="Your Website Here"
+                        />
+                        <button type="submit" onClick={this.updateProfile} className="btn">
+                            Save
+                        </button>
+                    </form>
+                </div>
+            </>
+        );
+    }
+    }
+
 
 export default UpdateUserProfile;
