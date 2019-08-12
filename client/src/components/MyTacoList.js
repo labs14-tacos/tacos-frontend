4264b598ae93da1ca41a4b737890e3f30fb67762
meginks <meganjones.dev@gmail.com>
2019-08-12 15:55:10 -0500

    made landing page once you log in look like mockups

---
+++ client/src/components/MyTacoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyTacoList extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default MyTacoList;
---
+++ client/src/components/OthersTacos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class OthersTacos extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default OthersTacos;
