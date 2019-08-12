153b863ecef4473c76a68125eb6e00df37424bc9
meginks <meganjones.dev@gmail.com>
2019-08-12 16:35:16 -0500

    created view my tacos and view other people's tacos views

--- client/src/components/MyTacoList.js
+++ client/src/components/MyTacoList.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import {Link as RouterLink } from 'react-router-dom';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-class MyTacoList extends React.Component {
+
+class MyTacoFeed extends React.Component {
+    state = {
+        tacofeed: []
+    }
+
+    componentDidMount() {
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/tacolog/mytacolog`).then(
+            res => {
+                this.setState({tacofeed: res.data})
+            }
+        ).catch(error => console.log(error))
+    }
+
     render() {
+        console.log(this.state.tacofeed, "tacofeed");
         return (
             <div>
-
+            <ButtonGroup  
+              aria-label="full-width contained primary button group">
+                <Button component={RouterLink} to="/my-tacos" color="primary" variant="contained" >
+                    My Tacos
+                </Button>
+                <Button component={RouterLink} to="/explore-tacos" color="primary" variant="outlined"> 
+                    Other People's Tacos
+                </Button>
+            </ButtonGroup>
+            <GridList>
+            {this.state.tacofeed.map(taco => <GridListTile key={taco.img}><img src={taco.tacoLogPhoto} alt={taco.nameOfTaco}/></GridListTile>)}
+            </GridList>
             </div>
         )
     }
 }
 
-export default MyTacoList;
+export default MyTacoFeed;
--- client/src/components/OthersTacos.js
+++ client/src/components/OthersTacos.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import {Link as RouterLink } from 'react-router-dom';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+
 
 class OthersTacos extends React.Component {
+    state = {
+        tacofeed: []
+    }
+
+    componentDidMount() {
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/tacolog`).then(
+            res => {
+                this.setState({tacofeed: res.data})
+            }
+        ).catch(error => console.log(error))
+    }
+
     render() {
         return (
             <div>
-
+            <ButtonGroup  
+              aria-label="full-width contained primary button group">
+                <Button component={RouterLink} to="/my-tacos" color="primary" variant="outlined">
+                    My Tacos
+                </Button>
+                <Button component={RouterLink} to="/explore-tacos" color="primary" variant="contained"> 
+                    Other People's Tacos
+                </Button>
+            </ButtonGroup>
+            <GridList>
+            {this.state.tacofeed.map(taco => <GridListTile key={taco.id}><img src={taco.tacoLogPhoto} alt={taco.nameOfTaco}/></GridListTile>)}
+            </GridList>
             </div>
         )
     }
