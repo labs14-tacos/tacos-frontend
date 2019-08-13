8f5ea49231aa6ccb4a2f4249f0d51d058d65ff06
anthony <anthony24.kwhomes@gmail.com>
2019-08-13 15:46:12 -0700

    Doing a pull. Working on tacoView.

--- client/src/components/Taco.js
+++ client/src/components/Taco.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import ButtonGroup from '@material-ui/core/Button';
+import axios from 'axios';
 
-const Taco = props => {
-    return (
-        <div className="taco">
-            <h2>{props.restaurantName}</h2>
-            <h2>{props.date}<h2>
-            <h2>{props.totalTacos}</h2>
-            <h1>{props.userPhoto}</h1>
-            <h2>{props.tacoName}</h2>
-            <h2>{props.description}<h2>
-            <h2>{props.rating}</h2>
-            <h2>{props.comments}</h2>
-        </div>
-    );
-};
+class Taco extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            taco: null
+        };
+    }
 
-Taco.defaultProps = {
-    restaurantName: "",
-    date: "",
-    totalTacos: "",
-    userPhoto: "",
-    tacoName: "",
-    description: "",
-    rating: "",
-    comments: ""
-};
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        this.fetchTaco(this.props.match.params.id);
+    }
+
+    fetchTaco = id => {
+        axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/tacolog/${id}`)
+        .then(res => {
+            this.setState(() => ({ taco: res.data }));
+        })
+        .catch(error => console.log(error))
+    }
+
+    render() {
+        if (!this.state.taco) {
+            return <div>Loading Taco information...</div>
+        }
+
+        const { restauant, date, totalTacos, tacoName, description, rating, comments } = this.state.taco;
+        return (
+            <div>
+                <div className="taco-card">
+                    <h2>{restauant}</h2>
+                    <div className="date">
+                        <h2>{date}</h2>
+                    </div>
+                    <div className="total-tacos">
+                        Total Tacos: <strong>{totalTacos}</strong>
+                    </div>
+                    <div className="taco-name">
+                        Taco Name: <strong>{tacoName}</strong>
+                    </div>
+                    <div className="description">
+                        <h2>{description}</h2>
+                    </div>
+                    <div className="rating">
+                        <h2>{rating}</h2>
+                    </div>
+                    <div className="comments">
+                        <h2>{comments}</h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Taco;
