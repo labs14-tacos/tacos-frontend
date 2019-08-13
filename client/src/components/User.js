aa5d25aec6f8ce5d4eaf5352ad81cf8275770598
meginks <meganjones.dev@gmail.com>
2019-08-13 11:36:51 -0500

    user profile is pulling in profile data

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,8 +1,10 @@
 import React from 'react'; 
 // import UserAvatar from './UserAvatar';
-import Paper from '@material-ui/core/Paper';
+import {Paper, Button} from '@material-ui/core/';
+import {Link as RouterLink} from 'react-router-dom';
 import Axios from 'axios';
 
+const token = sessionStorage.getItem("token");
 
 // This can be used to display the person who is logged in's own profile information
 class User extends React.Component {
@@ -12,15 +14,25 @@ class User extends React.Component {
 
 
     componentDidMount() {
-        Axios.get(`${process.env.REACT_APP_BACKEND_URL}/my_profile`).then(user => this.setState({user})).catch(error=> console.log(error));
+        Axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/my_info`, {headers: {token: `${token}`}}).then(user => this.setState({user: user.data})).catch(error=> console.log(error));
     }
     render() {
+        console.log(this.state.user, "user console log")
     return (
         <div>
                <Paper>
-                <img src={this.state.user.photoURL}/>
+                <img className="avatar-image" src={this.state.user.userPhoto}/>
                 <h1>{this.state.user.firstName} {this.state.user.lastName}</h1> 
-
+                <p>Email: {this.state.user.email}</p>
+                <p>Instagram: {this.state.user.instaHandle}</p>
+                <p>Twitter: {this.state.user.twitterHandle}</p>
+                <p>Facebook: {this.state.user.facebookPage}</p> 
+                <p>Website: {this.state.user.website}</p>
+                <div className="button-container">
+        <Button component={RouterLink} to="/my-tacos" variant="contained" size="medium" color="primary">
+          My Tacos
+        </Button>
+        </div>
                </Paper>
         </div>
     )
