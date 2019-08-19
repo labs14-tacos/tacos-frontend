e6118592e3036ecaebce5e96eca8de578d944527
Raine Wallace <rainebeaudubs@gmail.com>
2019-08-19 17:03:47 -0400

    fixed totalTacos issues, made a tocolog field required.

--- client/src/components/MyTaco.js
+++ client/src/components/MyTaco.js
@@ -7,6 +7,8 @@ import ButtonGroup from '@material-ui/core/Button';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
+
+
 const token = sessionStorage.getItem("token")
 
 class MyTaco extends Component {
@@ -15,7 +17,7 @@ class MyTaco extends Component {
         this.state = {
             taco: {},
             taco_id: null,
-            taco_ingredients: {protein: [], topping: [], salsa: [], cheese: [], tortilla: []}
+            taco_ingredients: { protein: [], topping: [], salsa: [], cheese: [], tortilla: [] }
         };
     };
 
@@ -23,33 +25,34 @@ class MyTaco extends Component {
         const tacoId = this.props.location.state.id
         const ing = this.props.location.state.ingredients
         const ingredientObject = JSON.parse(ing);
-        this.setState({taco_id: tacoId, taco_ingredients: ingredientObject});
+        this.setState({ taco_id: tacoId, taco_ingredients: ingredientObject });
         this.fetchTaco(this.props.location.state.id);
         console.log(this.props.location.state.id, 'taco');
     }
 
+
     fetchTaco = id => {
         axios
-        .get(`${process.env.REACT_APP_BACKEND_URL}/tacolog/${id}`, {headers: {token: token}})
-        .then(res => {
-            this.setState({ taco: res.data });
-            console.log(this.state.taco, 'fetchTaco')
+            .get(`${process.env.REACT_APP_BACKEND_URL}/tacolog/${id}`, { headers: { token: token } })
+            .then(res => {
+                this.setState({ taco: res.data });
+                console.log(this.state.taco, 'fetchTaco')
 
-        })
-        .catch(error => console.log(error, "fetchTacoError"))
+            })
+            .catch(error => console.log(error, "fetchTacoError"))
     }
 
     onDelete() {
         // let id = this.state.taco_id;
         console.log(this.state.taco.id)
-        axios.delete(`${process.env.REACT_APP_BACKEND_URL}/tacolog/${this.props.location.state.id}`, {headers: {token: token}})
-        .then(response => {
-            console.log('delete working', response)
-            // this.props.history.push('/my-tacos');
-        }) .catch(err => console.log(err));
+        axios.delete(`${process.env.REACT_APP_BACKEND_URL}/tacolog/${this.props.location.state.id}`, { headers: { token: token } })
+            .then(response => {
+                console.log('delete working', response)
+                // this.props.history.push('/my-tacos');
+            }).catch(err => console.log(err));
     }
 
-    
+
     render() {
         console.log(this.state.taco, 'tacoState')
 
@@ -57,7 +60,13 @@ class MyTaco extends Component {
             return <div>Loading Taco information...</div>
 
         }
-        const { restaurantName, date, totalTacos, nameOfTaco, ingredients, protein, cheese, salsa, topping, rating, notes, tacoLogPhoto, t_rating, a_rating, c_rating, o_rating } = this.state.taco;
+        const { restaurantName, date, numberOfTacos, nameOfTaco, ingredients, protein, cheese, salsa, topping, rating, notes, tacoLogPhoto, t_rating, a_rating, c_rating, o_rating } = this.state.taco;
+
+        // const tacoDate = new Intl.DateTimeFormat('en-GB', { 
+        //     year: 'numeric', 
+        //     month: 'long', 
+        //     day: '2-digit' 
+        // }).format(this.state.taco.date)
 
         console.log(this.state)
 
@@ -69,62 +78,61 @@ class MyTaco extends Component {
                         <h2>{date}</h2>
                     </div>
                     <div className="total-tacos">
-                        Total Tacos: <strong>{totalTacos}</strong>
+                        Total Tacos: <strong>{numberOfTacos}</strong>
                     </div>
-                    <img src={tacoLogPhoto} alt={nameOfTaco}/>
+                    <img src={tacoLogPhoto} alt={nameOfTaco} />
                     <div className="taco-name">
                         Taco Name: <strong>{nameOfTaco}</strong>
                     </div>
                     <div className="description">
                         <h2>Description:</h2>
-                        <p>{this.state.taco_ingredients.protein[0]}</p>
-                          {this.state.taco_ingredients.tortilla.map(function(tortilla) {return <p>{tortilla}</p>})} 
-        {this.state.taco_ingredients.protein.map(function(protein)  {return <p>{protein}</p>})}
-                         {this.state.taco_ingredients.topping.map(function(topping) {return <p>{topping}</p>})}
-                        {this.state.taco_ingredients.cheese.map(function(cheese) {return <p>{cheese}</p>})}
-                        {this.state.taco_ingredients.salsa.map(function(salsa) {return <p>{salsa}</p>})} 
-                        
+                        {this.state.taco_ingredients.tortilla.map(function (tortilla) { return <p>{tortilla}</p> })}
+                        {this.state.taco_ingredients.protein.map(function (protein) { return <p>{protein}</p> })}
+                        {this.state.taco_ingredients.topping.map(function (topping) { return <p>{topping}</p> })}
+                        {this.state.taco_ingredients.cheese.map(function (cheese) { return <p>{cheese}</p> })}
+                        {this.state.taco_ingredients.salsa.map(function (salsa) { return <p>{salsa}</p> })}
+
                     </div>
-                    <div className="rating">
-                    <h1>"Overall Rating"</h1>
-                     <Rating
-                    name= 'rating'
-                    value={rating}
-                    />
+                    <div className="ratings">
+                        <h1>"Overall Rating"</h1>
+                        <Rating
+                            name='rating'
+                            value={rating}
+                        />
 
-                    <h3>"T" Rating: {t_rating}</h3>
-                    <h4>"<span>T</span>he Fundamentals"</h4>
-                    <Rating
-                    name='t_rating'
-                    value={t_rating}
-                    />
-                    <h3>"A" Rating: {a_rating}</h3>
-                    <h4>"<span>A</span>lways Different, Positive, Special"</h4>
+                        <h3>"T" Rating: {t_rating}</h3>
+                        <h4>"<span>T</span>he Fundamentals"</h4>
+                        <Rating
+                            name='t_rating'
+                            value={t_rating}
+                        />
+                        <h3>"A" Rating: {a_rating}</h3>
+                        <h4>"<span>A</span>lways Different, Positive, Special"</h4>
 
-                     <Rating
-                    name='a_rating'
-                    value={a_rating}
-                    />
-                    <h3>"C" Rating: {c_rating}</h3>
-                    <h4>"<span>C</span>onsistent Commitment"</h4>
+                        <Rating
+                            name='a_rating'
+                            value={a_rating}
+                        />
+                        <h3>"C" Rating: {c_rating}</h3>
+                        <h4>"<span>C</span>onsistent Commitment"</h4>
 
-                     <Rating
-                    name='c_rating'
-                    value={c_rating}
-                    />
-                    <h3>"O" Rating: {o_rating}</h3>
-                    <h4>"<span>O</span>h, Wow!"</h4>
-                     <Rating
-                    name='o_rating'
-                    value={o_rating}
-                    />
+                        <Rating
+                            name='c_rating'
+                            value={c_rating}
+                        />
+                        <h3>"O" Rating: {o_rating}</h3>
+                        <h4>"<span>O</span>h, Wow!"</h4>
+                        <Rating
+                            name='o_rating'
+                            value={o_rating}
+                        />
                     </div>
                     <div className="comments">
                         <h2>{notes}</h2>
                     </div>
                     <ButtonGroup>
-                    <Button onClick={() =>this.onDelete()} id="primaryBtn" component={RouterLink} to="/my-tacos" color="primary" variant="contained"> 
-                        Delete
+                        <Button onClick={() => this.onDelete()} id="primaryBtn" component={RouterLink} to="/my-tacos" color="primary" variant="contained">
+                            Delete
                     </Button>
                     </ButtonGroup>
                 </div>
