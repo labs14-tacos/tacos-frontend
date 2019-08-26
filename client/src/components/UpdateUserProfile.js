8677e82c783b778536b15742d0af3206c4aa143d
anthony <anthony24.kwhomes@gmail.com>
2019-08-25 19:29:58 -0700

    added content and fixed a styling issue in the User Profile edit.

--- client/src/components/UpdateUserProfile.js
+++ client/src/components/UpdateUserProfile.js
@@ -1,8 +1,11 @@
 import React from 'react'; 
 import PhotoUpload from './cloudinary/UserTacoImage'
-import {Paper, Button, TextField} from '@material-ui/core/';
+import {Paper, Button, TextField, Container} from '@material-ui/core/';
 import Axios from 'axios';
 import {Link as RouterLink} from 'react-router-dom';
+import Rating from '@material-ui/lab/Rating';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+
 
 const token = sessionStorage.getItem("token");
 
@@ -20,6 +23,14 @@ class UpdateUserProfile extends React.Component {
            twitterHandle: '',
            instaHandle: '',
            website: '',
+           zipcode: '',
+           tacosPerMonth: '',
+           hardOrSoft: '',
+           cornOrFlour: '',
+           heatPreference: '',
+           streetOrGourmet: '',
+           favTacoLocation: '',
+           bestTacoMemory: '',
          
         };
     }
@@ -37,6 +48,15 @@ class UpdateUserProfile extends React.Component {
             twitterHandle: userInfo.twitterHandle,
             instaHandle: userInfo.instaHandle,
             website: userInfo.website,
+            zipcode: userInfo.zipcode,
+            tacosPerMonth: userInfo.tacosPerMonth,
+            hardOrSoft: userInfo.hardOrSoft,
+            cornOrFlour: userInfo.cornOrFlour,
+            heatPreference: userInfo.heatPreference,
+            streetOrGourmet: userInfo.streetOrGourmet,
+            favTacoLocation: userInfo.favTacoLocation,
+            bestTacoMemory: userInfo.bestTacoMemory
+
           
         });
          console.log("componentDidMount in update profile", userInfo);
@@ -90,7 +110,7 @@ class UpdateUserProfile extends React.Component {
                
                   {/* <button onClick={() => this.setState(prevState => ({changePhoto: !prevState.changePhoto}))}>Change my photo</button>  */}
                 </div>
-                <div className="flex-container">
+                <Container>
                 
                 <TextField
                             label="First Name"
@@ -157,8 +177,68 @@ class UpdateUserProfile extends React.Component {
                             onChange={this.handleChange}
                             placeholder="Favorite Taco"
                 />
+                <TextField
+                            label="Zip Code"
+                            name="zipcode"
+                            value={this.state.zipcode}
+                            onChange={this.handleChange}
+                            placeholder="Zip Code"
+                />
+                <TextField
+                            label="Tacos Per Month"
+                            name="tacosPerMonth"
+                            value={this.state.tacosPerMonth}
+                            onChange={this.handleChange}
+                            placeholder="Tacos Per Month"
+                />
+                <TextField
+                            label="Hard Or Soft"
+                            name="hardOrSoft"
+                            value={this.state.hardOrSoft}
+                            onChange={this.handleChange}
+                            placeholder="Hard Or Soft"
+                />
+                <TextField
+                            label="Corn Or Flour"
+                            name="cornOrFlour"
+                            value={this.state.cornOrFlour}
+                            onChange={this.handleChange}
+                            placeholder="Corn Or Flour"
+                />
+                <Container className="ratings">
+                    <p>Heat Preference:</p>
+                <Rating className="rating"
+                            icon={<WhatshotIcon fontsize="inherit" />}
+                            label="Heat Preference"
+                            name="heatPreference"
+                            value={this.state.heatPreference}
+                            onChange={this.handleChange}
+                            placeholder="Heat Preference"
+                />
+                </Container>
+                <TextField
+                            label="Street Or Gourmet"
+                            name="streetOrGourmet"
+                            value={this.state.streetOrGourmet}
+                            onChange={this.handleChange}
+                            placeholder="Street Or Gourmet"
+                />
+                <TextField
+                            label="Favorite Taco Location"
+                            name="favTacoLocation"
+                            value={this.state.favTacoLocation}
+                            onChange={this.handleChange}
+                            placeholder="Favorite Taco Location"
+                />
+                <TextField
+                            label="Best Taco Memory"
+                            name="bestTacoMemory"
+                            value={this.state.bestTacoMemory}
+                            onChange={this.handleChange}
+                            placeholder="Best Taco Memory"
+                />
          
-                </div>
+                </Container>
                 <Button id='proBtn' type="submit" onClick={this.updateProfile} >
                     Save
                 </Button>
